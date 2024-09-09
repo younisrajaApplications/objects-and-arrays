e04d9d1cdb7c4fb65862586367f1d1fe4436625e
younisrajaApplications <younisraja.applications@gmail.com>
2024-09-09 17:42:10 +0100

    Show to do list on the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import UserForm from './UserForm.js';
+import TodoApp from './TodoApp.js';
 
 function App() {
   return (
     <div className="App">
       <UserForm/>
+      <br/>
+      <TodoApp/>
     </div>
   );
 }
